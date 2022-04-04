bda7105218e37272c10a56d90bef9e1d28476a3f
robertozecchini <robertozecchini@gmail.com>
2022-04-04 08:54:50 +0200

    work in progress: trying to read from socket to console.log

--- frontend/src/components/ChatComponent/ChatComponent.js
+++ frontend/src/components/ChatComponent/ChatComponent.js
@@ -5,12 +5,20 @@ import './ChatComponent.css';
 import {io} from 'socket.io-client';
 
 function ChatComponent() {
+
   const socket = io('ws://sanchochat-backend.herokuapp.com/');
+
   function sendMessage(event) {
     event.preventDefault()
     socket.emit("chat message", event.target.message.value)
     console.log("submit eseguito: " + event.target.message.value)
   }
+
+  // receive a message from the server
+  socket.addEventListener("chat message", ( data ) => {
+    console.log(data)
+  });
+
   return (
     <div>
         <h1>ChatComponent</h1>
