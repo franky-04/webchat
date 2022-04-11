e8829fb3d089af655f6befd16d541e98431e86d6
Giancarlo <giancarlourone4@gmail.com>
2022-04-11 08:55:12 +0200

    added some style, improved input functionality

--- frontend/src/components/ChatComponent/ChatComponent.js
+++ frontend/src/components/ChatComponent/ChatComponent.js
@@ -9,18 +9,12 @@ function ChatComponent() {
 
   const socket = io('ws://sanchochat-backend.herokuapp.com/');
 
-  function sendMessage(event) {
-    event.preventDefault()
-    socket.emit("chat message", event.target.message.value)
-    console.log("submit eseguito: " + event.target.message.value)
-  }
-
   return (
     <div className="container-fluid">
       <div className="center">
         <SocketContext.Provider value={socket}>
           <ConversationComponent />
-          <InputComponent onSubmit={sendMessage} />
+          <InputComponent />
         </SocketContext.Provider>
       </div>
     </div>
